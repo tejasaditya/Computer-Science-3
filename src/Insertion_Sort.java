6a417cb67acbbac2acb42957f8c64fa05db60fa8
Tejas Aditya <tejasaditya@me.com>
2019-01-09 17:52:15 -0500

    Insertion Sort Coding Challenge

---
+++ src/Insertion_Sort.java
@@ -0,0 +1,39 @@
+public class Insertion_Sort
+{
+    public static void main(String[] args)
+    {
+        int[] array = {9,8,7,6,5,4,3,2,1};
+        System.out.println("Original Array");
+        for(int i = 0;i<array.length;i++)
+        {
+                System.out.print(array[i]);
+        }
+
+        System.out.println("");
+        System.out.println("Sorted Array");
+        array_sort(array);
+        for (int i = 0;i<array.length;i++)
+        {
+                System.out.print(array[i]);
+        }
+
+    }
+    public static void array_sort(int[] array)
+    {
+        int length = array.length;
+        int i, current, compare_id;
+        for (i = 1; i<length;i++)
+        {
+            current = array[i];
+            compare_id = i-1;
+
+            while(compare_id>=0 && array[compare_id]>current)
+            {
+                int temp = array[compare_id+1];
+                array[compare_id+1] = array[compare_id];
+                array[compare_id] = temp;
+                compare_id --;
+            }
+        }
+    }
+}
